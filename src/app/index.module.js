4773b6c65ab80070b3414413908e0b924914266b
Enninful, George <enninful@hotmail.com>
2016-07-24 23:03:26 -0700

    Initial commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('testApp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'restangular', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
